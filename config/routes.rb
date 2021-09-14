c2231bc68fe5fb206fa6ce8ed95c83a4b3423edf
auriane gregoire <auriane.gregoire@gmail.com>
2021-09-14 19:11:08 +0200

    page contact : bug js tant que pas de site sécurisé à cause de typeform >> de coté pour le moment en prod

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :albums
 
   root to: 'pages#home'
-  get 'pages/contact'
+  # get 'pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
