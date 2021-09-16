e0fb07d4d4257bf0e044a81119981415076f5705
auriane gregoire <auriane.gregoire@gmail.com>
2021-09-16 17:01:07 +0200

    home js slider

--- app/javascript/plugins/slider_home.js
+++ app/javascript/plugins/slider_home.js
@@ -1,23 +1,59 @@
 const sliderHome = () => {
 
   const slides = document.querySelectorAll('.carroussel-item');
-  let index = 0;
+  let index = null;
+  const leftBtn = document.querySelector('.swiper-button-prev');
+  const rightBtn = document.querySelector('.swiper-button-next');
+  let active = document.querySelector('.active');
+
 
   if (slides.length < 2) { return };
 
-  setInterval(function(){
-    if(index < 1){
-      slides[index].classList.remove('active');
-      index++;
-      slides[index].classList.add('active');
-    }
-    else if (index === 1) {
-      slides[index].classList.remove('active');
-      index = 0;
-      slides[index].classList.add('active');
-    }
-   }, 5000);
+  slides.forEach((item) => { 
+    item.number = index + 1;
+    index ++; 
+  })
 
+
+  const changeSlideRight = () => {
+
+    if (active === slides[slides.length - 1]) {
+        active.classList.remove('active');
+        slides[0].classList.add('active');
+        active = document.querySelector('.active');
+    } else {
+      active.classList.remove('active');
+      slides[active.number].classList.add('active');
+      active = document.querySelector('.active');
+    }
+
+  }
+
+  const changeSlideLeft = () => {
+
+    if (active === slides[0]) {
+        active.classList.remove('active');
+        slides[slides.length - 1].classList.add('active');
+        active = document.querySelector('.active');
+    } else {
+      active.classList.remove('active');
+      slides[active.number - 2].classList.add('active');
+      active = document.querySelector('.active');
+    }
+
+  }
+
+setInterval(changeSlideRight, 5000);
+
+
+
+  leftBtn.addEventListener('click', () => {
+    changeSlideLeft();
+  });
+
+  rightBtn.addEventListener('click', () => {
+    changeSlideRight();
+  });
 
 
 
